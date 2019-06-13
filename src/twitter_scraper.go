be2dc434f41b813d936723741f26ba05ce668bab
McKade Umbenhower <mckade@localhost.localdomain>
2019-06-13 06:39:39 -0600

    updated scraper

--- src/twitter_scraper.go
+++ src/twitter_scraper.go
@@ -12,7 +12,8 @@ type item struct {
 
 func main() {
 	tweets := []item{}
-	// Instantiate default collector
+	var minTweetID string
+    // Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains: old.reddit.com
 		colly.AllowedDomains("twitter.com"),
@@ -25,9 +26,15 @@ func main() {
         temp := item{}
 		temp.text = e.ChildText("p[data-aria-label-part=\"0\"]")
 		tweets = append(tweets, temp)
-        fmt.Println(temp.text)
-        fmt.Println()
+        //fmt.Println(temp.text)
+        //fmt.Println()
 	})
+
+    c.OnHTML(".stream-container", func(e *colly.HTMLElement) {
+        minTweetID = e.Attr("data-min-position")
+        minTweetID = minTweetID[:len(minTweetID) - 1]
+        fmt.Printf("Min tweet ID: %s\n", minTweetID)
+    })
 
 	// Set max Parallelism and introduce a Random Delay
 	c.Limit(&colly.LimitRule{
@@ -42,9 +49,16 @@ func main() {
 
 	// Crawl tweets
 	btc := "https://twitter.com/search?f=tweets&vertical=news&q=bitcoin&src=typd&lang=en"
+    //btc := "https://twitter.com/search?f=tweets&vertical=news&q=bitcoin&src=typd&include_available_features=1&include_entities=1&max_position=thGAVUV0VFVBaAgLfZyIyxzx8WgoC35d2bsc8fEjUAFQAlAAA%3D&reset_error_state=false"
+    //btc:= "https://twitter.com/search?f=tweets&vertical=news&q=bitcoin&src=typd&include_available_features=1&include_entities=1&max_position=1139241875259482112&reset_error_state=false"
     c.Visit(btc)
-
     c.Wait()
+
+    for i := 0; i < 1; i++ {
+        btc := "https://twitter.com/search?f=tweets&vertical=news&q=bitcoin&src=typd&include_available_features=1&include_entities=1&max_position=" + minTweetID + "&reset_error_state=false"
+        c.Visit(btc)
+        c.Wait()
+    }
 
     // print results
     fmt.Printf("Number of tweets: %d\n", len(tweets))
