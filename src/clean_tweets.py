6313d0ef9d4aaa84ce0d222a444e76af39ad5691
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-19 11:57:19 -0400

    clean_tweets.py now formats differently.

--- src/clean_tweets.py
+++ src/clean_tweets.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import glob
 import json
+import os
 
-files = glob.glob('TweetScraper/Data/tweet/*')
+tweetscraper_dir = '/homes/3/whitaker.213/packages/TweetScraper'
+path = os.path.join(tweetscraper_dir, 'TweetScraper/Data/tweet/')
+print(path)
+assert os.path.exists(path)
+files = glob.glob(path + '*')
+print(files)
 
 dictlist = []
 
@@ -16,4 +22,13 @@ df = df.replace({'\n': ' '}, regex=True)
 df = df.replace({'\t': ' '}, regex=True)
 df = df.replace({'\r': ' '}, regex=True)
 
-df.to_csv("data.tsv", sep = '\t')
+df['index'] = df.index
+print(df.columns)
+df = df.drop(labels=['ID', 'datetime', 'has_media', 'is_reply', 'is_retweet', 'medias', 'nbr_favorite', 'nbr_reply', 'nbr_retweet', 'url', 'user_id', 'usernameTweet'], axis=1)
+cols = df.columns.tolist()
+cols = cols[::-1]
+df = df[cols]
+df = df.rename(mapper={'text':'sentence'}, axis=1)
+print(df.head(10))
+
+df.to_csv("data.tsv", sep = '\t', index=False)
