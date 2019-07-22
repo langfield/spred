a318b93c8f11dcb045caf571bb77a58fe3416df4
McKade Umbenhower <mckade22@gmail.com>
2019-07-21 19:23:11 -0600

    files

---
+++ src/keras.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as pyplot
+
+stock_data = pd.read_csv('exchange/ETHUSDT.csv')
+stock_data = stock_data.drop(columns=['Date'])
+print(stock_data.head())
+
+sns.pairplot(stock_data, hue='Open')
