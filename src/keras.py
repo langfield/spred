7fd3af81f22909524ac65bafbc5b83c27b8a3fad
brendanxwhitaker <whitaker.213@osu.edu>
2019-07-21 21:29:07 -0400

    No big file.

--- src/keras.py
+++ src/keras.py
@@ -3,8 +3,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as pyplot
 
-stock_data = pd.read_csv('exchange/ETHUSDT.csv')
+stock_data = pd.read_csv('exchange/ETHUSDT_small.csv')
 stock_data = stock_data.drop(columns=['Date'])
 print(stock_data.head())
 
-sns.pairplot(stock_data, hue='Open')
+sns.pairplot(stock_data)
