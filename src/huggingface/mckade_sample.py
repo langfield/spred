a327b8993944bffe5dd1847e7bfe1f4f7c741b48
McKade Umbenhower <mckade22@gmail.com>
2019-07-23 12:57:24 -0600

    files

--- src/huggingface/mckade_sample.py
+++ src/huggingface/mckade_sample.py
@@ -8,11 +8,29 @@ print("Config file:", config)
 # config.save_pretrained("./")
 
 print('Getting tokenizer...')
-tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+#tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
 print('Getting model...')
 model = XLNetModel(config)
 print('Tokenizing input...')
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+#input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch 
+# shape(seq_length, batch_size, embedding_dim)
+input_ids = torch.tensor(
+    [[[ 0.0036,  0.0227],
+     [ 0.0036,  0.0227]],
+    [[ 0.0128,  0.0058],
+     [ 0.0128,  0.0058]],
+    [[ 0.0315,  0.0126],
+     [ 0.0315,  0.0126]],
+    [[ 0.0096, -0.0011],
+     [ 0.0096, -0.0011]],
+    [[ 0.0066,  0.0123],
+     [ 0.0066,  0.0123]],
+    [[ 0.0006,  0.0043],
+     [ 0.0006,  0.0043]],
+    [[-0.0346,  0.0088],
+     [-0.0346,  0.0088]]]
+    )
+# size 1
 print("input_ids:", input_ids)
 print('Getting output...')
 outputs = model(input_ids)
