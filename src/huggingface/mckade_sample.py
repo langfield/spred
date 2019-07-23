b3c9c69bb131a777610cb2d7ded99bcdda1b5fce
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-22 21:21:03 -0400

    HuggingFace samples.

---
+++ src/huggingface/mckade_sample.py
@@ -0,0 +1,15 @@
+import torch
+from pytorch_transformers import *
+
+print('Getting config...')
+config = XLNetConfig.from_pretrained('xlnet-base-cased')
+print('Getting tokenizer...')
+tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+print('Getting model...')
+model = XLNetModel(config)
+print('Tokenizing input...')
+input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+print('Getting output...')
+outputs = model(input_ids)
+last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+print(last_hidden_states)
