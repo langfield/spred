a6aa74488dd04eb23eff9b41deb37ab829e4e808
brendanxwhitaker <whitaker.213@osu.edu>
2019-07-23 14:58:29 -0400

    Embeddings commented out.

--- src/huggingface/mckade_sample.py
+++ src/huggingface/mckade_sample.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import torch
 from pytorch_transformers import *
@@ -12,7 +13,30 @@ tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
 print('Getting model...')
 model = XLNetModel(config)
 print('Tokenizing input...')
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+
+print('Tokenizer encode output:', tokenizer.encode("Hello, my dog is cute"))
+seq = tokenizer.encode("Hello, my dog is cute")
+input_ids = torch.tensor([seq, copy.deepcopy(seq)])  # Batch size 1
+input_ids =  torch.tensor([[[-0.0050, -0.0232],
+         [-0.0050, -0.0232]],
+
+        [[ 0.0358, -0.0225],
+         [ 0.0358, -0.0225]],
+
+        [[ 0.0110,  0.0015],
+         [ 0.0110,  0.0015]],
+
+        [[ 0.0368,  0.0127],
+         [ 0.0368,  0.0127]],
+
+        [[ 0.0081, -0.0139],
+         [ 0.0081, -0.0139]],
+
+        [[-0.0427, -0.0104],
+         [-0.0427, -0.0104]],
+
+        [[ 0.0076, -0.0234],
+         [ 0.0076, -0.0234]]])
 print("input_ids:", input_ids)
 print('Getting output...')
 outputs = model(input_ids)
