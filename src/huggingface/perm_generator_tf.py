4ac6a9080dabeb3c7340baa1535858eb923d7a07
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-23 21:51:35 -0400

    Added print statements.

--- src/huggingface/perm_generator_tf.py
+++ src/huggingface/perm_generator_tf.py
@@ -27,44 +27,55 @@ def _local_perm(inputs, targets, is_masked, perm_size, seq_len):
     index = tf.transpose(tf.reshape(index, [-1, perm_size]))
     index = tf.random_shuffle(index)
     index = tf.reshape(tf.transpose(index), [-1])
-    print(index.eval())
+    print("Permutation indices:", index.eval())
 
     # `perm_mask` and `target_mask`
     # non-functional tokens
     non_func_tokens = tf.logical_not(tf.logical_or(
     tf.equal(inputs, SEP_ID),
     tf.equal(inputs, CLS_ID)))
+    print("Non functional tokens:", non_func_tokens.eval())
     
     non_mask_tokens = tf.logical_and(tf.logical_not(is_masked), non_func_tokens)
+    print("Non masked tokens:", non_mask_tokens.eval())
     masked_or_func_tokens = tf.logical_not(non_mask_tokens)
+    print("masked or func tokens:", masked_or_func_tokens.eval())
     
     # Set the permutation indices of non-masked (& non-funcional) tokens to the
     # smallest index (-1):
     # (1) they can be seen by all other positions
     # (2) they cannot see masked positions, so there won"t be information leak
     smallest_index = -tf.ones([seq_len], dtype=tf.int64)
+    print("smallest_index:", smallest_index.eval())
     rev_index = tf.where(non_mask_tokens, smallest_index, index)
+    print("rev_index:", rev_index.eval())
     
     # Create `target_mask`: non-funcional and maksed tokens
     # 1: use mask as input and have loss
     # 0: use token (or [SEP], [CLS]) as input and do not have loss
     target_tokens = tf.logical_and(masked_or_func_tokens, non_func_tokens)
+    print("target_tokens:", target_tokens.eval())
     target_mask = tf.cast(target_tokens, tf.float32)
+    print("target_mask:", target_mask.eval())
     
     # Create `perm_mask`
     # `target_tokens` cannot see themselves
     self_rev_index = tf.where(target_tokens, rev_index, rev_index + 1)
+    print("self_rev_index:", self_rev_index.eval())
     
     # 1: cannot attend if i <= j and j is not non-masked (masked_or_func_tokens)
     # 0: can attend if i > j or j is non-masked
     perm_mask = tf.logical_and(
     self_rev_index[:, None] <= rev_index[None, :],
     masked_or_func_tokens)
+    print("perm_mask:", perm_mask.eval())
     perm_mask = tf.cast(perm_mask, tf.float32)
+    print("perm_mask:", perm_mask.eval())
     
     # new target: [next token] for LM and [curr token] (self) for PLM
     new_targets = tf.concat([inputs[0: 1], targets[: -1]],
     axis=0)
+    print("new_targets:", new_targets.eval())
     
     # construct inputs_k
     inputs_k = inputs
