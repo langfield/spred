7d16d2959937b1fad8d370e0c9780e03d5f55cb2
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-07-26 14:43:45 -0400

    Next bit.

--- src/huggingface/perm_generator_torch.py
+++ src/huggingface/perm_generator_torch.py
@@ -29,58 +29,81 @@ def _local_perm(inputs, targets, is_masked, perm_size, seq_len):
     # `perm_mask` and `target_mask`
     # non-functional tokens
     non_func_tokens = ~(inputs.eq(SEP_ID) | inputs.eq(CLS_ID))
-    
+    non_mask_tokens = ~is_masked & non_func_tokens
+    masked_or_func_tokens = ~non_mask_tokens
     return 
- 
-    # `perm_mask` and `target_mask`
-    # non-functional tokens
-    non_func_tokens = tf.logical_not(tf.logical_or(
-    tf.equal(inputs, SEP_ID),
-    tf.equal(inputs, CLS_ID)))
-    
-    non_mask_tokens = tf.logical_and(tf.logical_not(is_masked), non_func_tokens)
-    masked_or_func_tokens = tf.logical_not(non_mask_tokens)
+  
     
     # Set the permutation indices of non-masked (& non-funcional) tokens to the
     # smallest index (-1):
     # (1) they can be seen by all other positions
     # (2) they cannot see masked positions, so there won"t be information leak
-   
-    # EXAMPLE
-    # `seq_len` is 5.  
-    # `index` is a permutation of the form [4, 2, 1, 0, 3].
-    # `smallest_index` is a tensor of the form [-1, -1, -1, -1, -1]. 
-    # `non_mask_tokens` is a tensor of booleans of the form [True, True, True, True, False]
-    # `rev_index` is a copy of index where all positions which are `False` in `non_mask_tokens`
-    #       are set to -1. 
-    # `rev_index` is thus a tensor of the form [4, 2, 1, 0, -1]. 
-    smallest_index = -tf.ones([seq_len], dtype=tf.int64) # Just all -1s
+    """
+    An array of length `seq_len` of all `-1`s. 
+    
+    EXAMPLE:
+        [-1, -1, -1, -1, -1].
+    """
+    smallest_index = -tf.ones([seq_len], dtype=tf.int64)
+    print("smallest_index:", smallest_index.eval())
+
+    """
+    Gets the indices of the sequence via `index`, and sets all the
+    non-masked and non-functional tokens to `-1`.     
+    """
     rev_index = tf.where(non_mask_tokens, smallest_index, index)
-    # In `rev_index` the non-masked tokens are -1.    
- 
-    # Create `target_mask`: non-functional and masked tokens
+    print("rev_index:", rev_index.eval())
+    
+    # Create `target_mask`: non-funcional and maksed tokens
     # 1: use mask as input and have loss
     # 0: use token (or [SEP], [CLS]) as input and do not have loss
-    target_tokens = tf.logical_and(masked_or_func_tokens, non_func_tokens) # Gets masked, non-functional tokens. 
+    """
+    Masked or functional tokens and non-functional tokens. 
+    So these must be non-functional. 
+    Tokens in `masked_or_func_tokens` must be either masked, functional, or both. 
+    Thus if these are non-functional tokens, they must be masked. 
+    So they are exactly all the masked and non-functional tokens. 
+    """
+    target_tokens = tf.logical_and(masked_or_func_tokens, non_func_tokens)
+    print("target_tokens:", target_tokens.eval())
     target_mask = tf.cast(target_tokens, tf.float32)
+    print("target_mask:", target_mask.eval())
     
     # Create `perm_mask`
     # `target_tokens` cannot see themselves
-    
-    # The -1s are the non-masked, non-functional, the target tokens are masked, non-functional. 
+    """
+    If a token is a target, it keeps its original index, since it is masked and non-functional, 
+    so it cannot be non-masked, so it cannot be set to `-1` in `rev_index`. 
+    Otherwise, we increment the value from `rev_index`.
+    These are all non-masked tokens, and all functional tokens. If a token
+    happens to be non-masked and non-functional, it will be incremented from `-1` to `0`. 
+    Thus there will be no `-1`s in the final tensor. 
+    """
     self_rev_index = tf.where(target_tokens, rev_index, rev_index + 1)
-    # `self_rev_index` has original indices for masked, non-functional tokens, 0 for non-masked tokens, and original indices + 1 for masked, functional tokens. 
+    print("self_rev_index:", self_rev_index.eval())
     
     # 1: cannot attend if i <= j and j is not non-masked (masked_or_func_tokens)
     # 0: can attend if i > j or j is non-masked
+    """
+    `1` marks tokens we're masking, `0` marks tokens we are not masking. 
+    The token we are currently at (trying to predict) is at `i`, and the one we're trying to attend to is
+    at `j`. If `i <= j` in the permutation order, then we cannot see it, and if j is masked, we cannot see it. 
+    Otherwise, we can see it.  
+    """
+    print("self_rev_index[:, None]:", self_rev_index[:, None].eval())
+    print("rev_index[None, :]:", rev_index[None, :].eval())
+    print("<=", (self_rev_index[:, None] <= rev_index[None, :]).eval())
     perm_mask = tf.logical_and(
     self_rev_index[:, None] <= rev_index[None, :],
     masked_or_func_tokens)
+    print("perm_mask:", perm_mask.eval())
     perm_mask = tf.cast(perm_mask, tf.float32)
+    print("perm_mask:", perm_mask.eval())
     
     # new target: [next token] for LM and [curr token] (self) for PLM
     new_targets = tf.concat([inputs[0: 1], targets[: -1]],
     axis=0)
+    print("new_targets:", new_targets.eval())
     
     # construct inputs_k
     inputs_k = inputs
