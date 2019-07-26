fbd4072473efb535f7d6c280aff939a2bef114f5
McKade Umbenhower <mckade22@gmail.com>
2019-07-26 14:20:26 -0600

    more train.py

--- src/huggingface/train.py
+++ src/huggingface/train.py
@@ -40,6 +40,11 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
 logger = logging.getLogger(__name__)
 
 
+UNK_ID = -9996
+SEP_ID = -9997
+CLS_ID = -9998
+MASK_ID = -9999
+
 class XLSpredDataset(Dataset):
     def __init__(self, corpus_path, seq_len, encoding="utf-8", corpus_lines=None, on_memory=True):
         self.seq_len = seq_len
@@ -90,7 +95,7 @@ class XLSpredDataset(Dataset):
         cur_example = InputExample(guid=cur_id, sample=sample)
 
         # transform sample to features
-        cur_features = convert_example_to_features(cur_example, self.seq_len, self.tokenizer)
+        cur_features = convert_example_to_features(cur_example, self.seq_len)
 
         cur_tensors = (torch.tensor(cur_features.input_ids),
                        torch.tensor(cur_features.input_mask),
@@ -128,14 +133,12 @@ class InputFeatures(object):
         self.lm_label_ids = lm_label_ids
 
 
-def random_word(tokens, tokenizer):
+def random_word(tokens):
     """
     Masking some random tokens for Language Model task with probabilities as in the original BERT paper.
-    :param tokens: list of str, tokenized sentence.
-    :param tokenizer: Tokenizer, object used for tokenization (we need it's vocab here)
-    :return: (list of str, list of int), masked tokens and related labels for LM prediction
+    :param tokens: list of float features; our sequence data (Average, Volume, ...).
+    :return: (list of [float]); masked tokens for LM prediction
     """
-    output_label = []
 
     for i, token in enumerate(tokens):
         prob = random.random()
@@ -145,79 +148,38 @@ def random_word(tokens, tokenizer):
 
             # 80% randomly change token to mask token
             if prob < 0.8:
-                tokens[i] = "[MASK]"
+                tokens[i] = MASK_ID
 
             # 10% randomly change token to random token
+            # TODO: what are our 'vocab' items--should be a list of floats
+            # like (Average, Volume, ...) 
             elif prob < 0.9:
                 tokens[i] = random.choice(list(tokenizer.vocab.items()))[0]
 
             # -> rest 10% randomly keep current token
-
-            # append current token to output (we will predict these later)
-            try:
-                output_label.append(tokenizer.vocab[token])
-            except KeyError:
-                # For unknown words (should not occur with BPE vocab)
-                output_label.append(tokenizer.vocab["[UNK]"])
-                logger.warning("Cannot find token '{}' in vocab. Using [UNK] insetad".format(token))
         else:
             # no masking token (will be ignored by loss function later)
             output_label.append(-1)
 
-    return tokens, output_label
+    return tokens
 
 
 def convert_example_to_features(example, max_seq_length):
     """
-    Convert a raw sample (pair of sentences as tokenized strings) into a proper training sample with
+    Convert a raw sample into a proper training sample with
     IDs, LM labels, input_mask, CLS and SEP tokens etc.
-    :param example: InputExample, containing sentence input as strings and is_next label
+    :param example: InputExample, containing seequence input
     :param max_seq_length: int, maximum length of sequence.
-    :param tokenizer: Tokenizer
     :return: InputFeatures, containing all inputs and labels of one sample as IDs (as used for model training)
     """
 
-    tokens_a, t1_label = random_word(tokens_a, tokenizer)
-    tokens_b, t2_label = random_word(tokens_b, tokenizer)
-    # concatenate lm labels and account for CLS, SEP, SEP
-    lm_label_ids = ([-1] + t1_label + [-1] + t2_label + [-1])
-
-    # The convention in BERT is:
-    # (a) For sequence pairs:
-    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-    #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
-    # (b) For single sequences:
-    #  tokens:   [CLS] the dog is hairy . [SEP]
-    #  type_ids: 0   0   0   0  0     0 0
-    #
-    # Where "type_ids" are used to indicate whether this is the first
-    # sequence or the second sequence. The embedding vectors for `type=0` and
-    # `type=1` were learned during pre-training and are added to the wordpiece
-    # embedding vector (and position vector). This is not *strictly* necessary
-    # since the [SEP] token unambigiously separates the sequences, but it makes
-    # it easier for the model to learn the concept of sequences.
-    #
-    # For classification tasks, the first vector (corresponding to [CLS]) is
-    # used as as the "sentence vector". Note that this only makes sense because
-    # the entire model is fine-tuned.
-    tokens = []
-    segment_ids = []
-    tokens.append("[CLS]")
-    segment_ids.append(0)
-    for token in tokens_a:
-        tokens.append(token)
-        segment_ids.append(0)
-    tokens.append("[SEP]")
-    segment_ids.append(0)
-
-    assert len(tokens_b) > 0
-    for token in tokens_b:
-        tokens.append(token)
-        segment_ids.append(1)
-    tokens.append("[SEP]")
-    segment_ids.append(1)
-
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    # Mask random tokens (words) in the sequences and return the corresponding
+    # label (indices) list 
+    sample = random_word(example.sample)
+    # account for CLS, SEP 
+    # a label is an index for a vocab word
+    lm_label_ids = ([-1] + sample + [-1])
+    input_ids = ([CLS_ID] + sample + [SEP_ID])
 
     # The mask has 1 for real tokens and 0 for padding tokens. Only real
     # tokens are attended to.
@@ -227,31 +189,27 @@ def convert_example_to_features(example, max_seq_length):
     while len(input_ids) < max_seq_length:
         input_ids.append(0)
         input_mask.append(0)
-        segment_ids.append(0)
         lm_label_ids.append(-1)
 
     assert len(input_ids) == max_seq_length
     assert len(input_mask) == max_seq_length
-    assert len(segment_ids) == max_seq_length
     assert len(lm_label_ids) == max_seq_length
 
-    if example.guid < 5:
-        logger.info("*** Example ***")
-        logger.info("guid: %s" % (example.guid))
-        logger.info("tokens: %s" % " ".join(
-                [str(x) for x in tokens]))
-        logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-        logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-        logger.info(
-                "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-        logger.info("LM label: %s " % (lm_label_ids))
-        logger.info("Is next sentence label: %s " % (example.is_next))
+    # if example.guid < 5:
+    #     logger.info("*** Example ***")
+    #     logger.info("guid: %s" % (example.guid))
+    #     logger.info("tokens: %s" % " ".join(
+    #             [str(x) for x in tokens]))
+    #     logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+    #     logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+    #     logger.info(
+    #             "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+    #     logger.info("LM label: %s " % (lm_label_ids))
+    #     logger.info("Is next sentence label: %s " % (example.is_next))
 
     features = InputFeatures(input_ids=input_ids,
                              input_mask=input_mask,
-                             segment_ids=segment_ids,
-                             lm_label_ids=lm_label_ids,
-                             is_next=example.is_next)
+                             lm_label_ids=lm_label_ids)
     return features
 
 
