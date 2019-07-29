97b922db8588f8b0a3241e3912199cf5127c44ec
brendanxwhitaker <whitaker.213@osu.edu>
2019-07-29 16:45:40 -0400

    Target mapping example is cleaner.

--- src/huggingface/target_mapping_example.py
+++ src/huggingface/target_mapping_example.py
@@ -1,21 +1,33 @@
 import numpy as np
 import torch
 
-seq_len = 7
+# Params. 
+SEQ_LEN = 7
 
-perm = np.random.permutation(seq_len)
-bools = [True if elem % 2 == 0 else False for elem in perm]
-indices = torch.arange(0, seq_len)
-bool_target_mask = torch.Tensor(bools).byte()
-indices = indices[bool_target_mask]
-index_len = len(indices)
-inp = indices % seq_len
-inp_ = torch.unsqueeze(inp, 1)
-one_hot = torch.FloatTensor(index_len, seq_len).zero_()
-one_hot.scatter_(1, inp_, 1)
+# Equivalent to ``tf.one_hot(indices, seq_len)``. 
+def one_hot(indices: torch.ByteTensor, seq_len: int) -> torch.FloatTensor:
+    index_len = len(indices)
+    inp = indices % seq_len
+    inp_ = torch.unsqueeze(inp, 1)
+    one_hot = torch.FloatTensor(index_len, seq_len).zero_()
+    one_hot.scatter_(1, inp_, 1)
+    return one_hot
 
-print("Seq length:", seq_len)
-print("Bool target mask:", bool_target_mask)
-print("Original indices:", torch.arange(0, seq_len))
-print("Masked indices:", indices)
-print("Output one_hot:\n", one_hot)
+if __name__ == "__main__":
+    
+    # Setup.
+    perm = np.random.permutation(SEQ_LEN)
+    bools = [True if elem % 2 == 0 else False for elem in perm]
+    indices = torch.arange(0, SEQ_LEN)
+    bool_target_mask = torch.Tensor(bools).byte()
+    indices = indices[bool_target_mask]
+
+    # Function call. 
+    one_hot = one_hot(indices, SEQ_LEN)
+    
+    # Output. 
+    print("Seq length:", SEQ_LEN)
+    print("Bool target mask:", bool_target_mask)
+    print("Original indices:", torch.arange(0, SEQ_LEN))
+    print("Masked indices:", indices)
+    print("Output one_hot:\n", one_hot)
