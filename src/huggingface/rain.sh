a5671ad428ab6085fa0fd1acf41feedbea34c090
brendanxwhitaker <whitaker.213@osu.edu>
2019-07-29 15:00:10 -0400

    Added padding to target_mapping.

--- src/huggingface/rain.sh
+++ src/huggingface/rain.sh
@@ -1,2 +1,2 @@
 # python3 train.py --train_corpus ../exchange/ETHUSDT_small.csv --xlspred_model config.json --output_dir checkpoints --do_train --train_batch_size 4
-python3 train.py --train_corpus ../exchange/ETHUSDT_small.csv --xlspred_model config.json --output_dir checkpoints --do_train --train_batch_size 7 --no_cuda --max_seq_length 50 --num_predict 3 --reuse_len 25 --data_batch_size 1
+python3 train.py --train_corpus ../exchange/ETHUSDT_small.csv --xlspred_model config.json --output_dir checkpoints --do_train --train_batch_size 7 --no_cuda --max_seq_length 50 --num_predict 12 --reuse_len 25 --data_batch_size 1
