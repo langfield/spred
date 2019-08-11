a979fec890f7b34dcd5b76ac59ec513fa15bfe92
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-11 15:06:54 -0400

    Changed src to spred to be consistent with python package formatting convention.

--- spred/gpst/train.py
+++ spred/gpst/train.py
@@ -44,7 +44,8 @@ if torch.__version__[:5] == "0.3.1":
 else:
     from torch.utils.data import RandomSampler
 
-from pytorch_transformers import (AdamW, WarmupLinearSchedule, cached_path, WEIGHTS_NAME, CONFIG_NAME)
+from pytorch_transformers import (AdamW, WarmupLinearSchedule, cached_path, WEIGHTS_NAME,
+                                  CONFIG_NAME)
 from dataset import GPSTDataset
 from modeling_openai import OpenAIGPTLMHeadModel, OpenAIGPTConfig
 
@@ -52,9 +53,9 @@ DEBUG = False
 
 ROCSTORIES_URL = "https://s3.amazonaws.com/datasets.huggingface.co/ROCStories.tar.gz"
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def accuracy(out, labels):
@@ -63,12 +64,20 @@ def accuracy(out, labels):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='openai-gpt',
+    parser.add_argument('--model_name',
+                        type=str,
+                        default='openai-gpt',
                         help='pretrained model name')
     parser.add_argument("--do_train", action='store_true', help="Whether to run training.")
-    parser.add_argument("--do_eval", action='store_true', help="Whether to run eval on the dev set.")
-    parser.add_argument("--output_dir", default=None, type=str, required=True,
-                        help="The output directory where the model predictions and checkpoints will be written.")
+    parser.add_argument("--do_eval",
+                        action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument(
+        "--output_dir",
+        default=None,
+        type=str,
+        required=True,
+        help="The output directory where the model predictions and checkpoints will be written.")
     parser.add_argument('--train_dataset', type=str, default='')
     parser.add_argument('--eval_dataset', type=str, default='')
     parser.add_argument('--seed', type=int, default=42)
@@ -82,11 +91,20 @@ def main():
     parser.add_argument('--weight_decay', type=float, default=0.01)
     parser.add_argument('--lm_coef', type=float, default=0.9)
     parser.add_argument('--n_valid', type=int, default=374)
-    parser.add_argument('--server_ip', type=str, default='', help="Can be used for distant debugging.")
-    parser.add_argument('--server_port', type=str, default='', help="Can be used for distant debugging.")
+    parser.add_argument('--server_ip',
+                        type=str,
+                        default='',
+                        help="Can be used for distant debugging.")
+    parser.add_argument('--server_port',
+                        type=str,
+                        default='',
+                        help="Can be used for distant debugging.")
 
     # Added.
-    parser.add_argument("--gpst_model", default=None, type=str, required=True,
+    parser.add_argument("--gpst_model",
+                        default=None,
+                        type=str,
+                        required=True,
                         help="OpenAIGPT pre-trained model path")
     args = parser.parse_args()
     print(args)
@@ -129,25 +147,33 @@ def main():
     train_data = GPSTDataset(args.train_dataset, max_length)
     print("Length of training dataset:", len(train_data))
     train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.train_batch_size)
+    train_dataloader = DataLoader(train_data,
+                                  sampler=train_sampler,
+                                  batch_size=args.train_batch_size)
 
     # Prepare optimizer
     if args.do_train:
         param_optimizer = list(model.named_parameters())
         no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-            ]
+        optimizer_grouped_parameters = [{
+            'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+            'weight_decay':
+            0.01
+        }, {
+            'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+            'weight_decay':
+            0.0
+        }]
         num_train_optimization_steps = len(train_dataloader) * args.num_train_epochs
-        optimizer = AdamW(optimizer_grouped_parameters,
-                               lr=args.learning_rate,
-                               #warmup=args.warmup_proportion,
-                               #max_grad_norm=args.max_grad_norm,
-                               weight_decay=args.weight_decay)
+        optimizer = AdamW(
+            optimizer_grouped_parameters,
+            lr=args.learning_rate,
+                                                                                      #warmup=args.warmup_proportion,
+                                                                                      #max_grad_norm=args.max_grad_norm,
+            weight_decay=args.weight_decay)
         scheduler = WarmupLinearSchedule(optimizer,
                                          warmup_steps=(args.warmup_proportion *
-                                         num_train_optimization_steps),
+                                                       num_train_optimization_steps),
                                          t_total=num_train_optimization_steps)
 
     if args.do_train:
@@ -199,10 +225,11 @@ def main():
                 if torch.__version__[:5] == "0.3.1":
                     loss_data = float(loss.data)
                     tr_loss += loss_data
-                    exp_average_loss = loss_data if exp_average_loss is None else 0.7*exp_average_loss+0.3*loss_data
+                    exp_average_loss = loss_data if exp_average_loss is None else 0.7 * exp_average_loss + 0.3 * loss_data
                 else:
                     tr_loss += loss.item()
-                    exp_average_loss = loss.item() if exp_average_loss is None else 0.7*exp_average_loss+0.3*loss.item()
+                    exp_average_loss = loss.item(
+                    ) if exp_average_loss is None else 0.7 * exp_average_loss + 0.3 * loss.item()
 
                 nb_tr_steps += 1
                 tqdm_bar.desc = "Training loss: {:.2e}".format(exp_average_loss)
@@ -210,7 +237,8 @@ def main():
     # Save a trained model
     if args.do_train:
         # Save a trained model, configuration and tokenizer
-        model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
+        model_to_save = model.module if hasattr(model,
+                                                'module') else model   # Only save the model it-self
 
         # If we save using the predefined names, we can load using `from_pretrained`
         output_model_file = os.path.join(args.output_dir, WEIGHTS_NAME)
