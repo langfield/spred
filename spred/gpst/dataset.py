7192988af73575858b3c8cc20dd33623a20e5519
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-11 15:41:03 -0400

    Linting in progress.

--- spred/gpst/dataset.py
+++ spred/gpst/dataset.py
@@ -1,19 +1,16 @@
+import copy
 import numpy as np
 import pandas as pd
 
-import copy
 import torch
 from torch.utils.data import Dataset
 
 DEBUG = True
 SIN = True
 
+
 class GPSTDataset(Dataset):
-    def __init__(self, 
-                 corpus_path, 
-                 seq_len, 
-                 encoding="utf-8", 
-                 on_memory=True):
+    def __init__(self, corpus_path, seq_len, encoding="utf-8", on_memory=True):
 
         self.seq_len = seq_len
 
@@ -22,26 +19,28 @@ class GPSTDataset(Dataset):
         self.encoding = encoding
 
         if SIN:
-            self.raw_data = pd.read_csv('sin.csv')
-            self.tensor_data = np.array(self.raw_data.iloc[:,:].values)
+            self.raw_data = pd.read_csv("sin.csv")
+            self.tensor_data = np.array(self.raw_data.iloc[:, :].values)
         else:
             # Load samples into memory from file.
             self.raw_data = pd.read_csv(corpus_path)
 
             # Add and adjust columns.
-            self.raw_data["Average"] = (self.raw_data["High"] + self.raw_data["Low"])/2
-            self.raw_data['Volume'] = self.raw_data['Volume'] + 0.000001 # Avoid NaNs
-            self.raw_data["Average_ld"] = (np.log(self.raw_data['Average']) - 
-                                        np.log(self.raw_data['Average']).shift(1))
-            self.raw_data["Volume_ld"] = (np.log(self.raw_data['Volume']) - 
-                                    np.log(self.raw_data['Volume']).shift(1))
+            self.raw_data["Average"] = (self.raw_data["High"] +
+                                        self.raw_data["Low"]) / 2
+            self.raw_data["Volume"] = self.raw_data["Volume"] + 0.000001  # Avoid NaNs
+            self.raw_data["Average_ld"] = np.log(self.raw_data["Average"]) - np.log(
+                self.raw_data["Average"]).shift(1)
+            self.raw_data["Volume_ld"] = np.log(self.raw_data["Volume"]) - np.log(
+                self.raw_data["Volume"]).shift(1)
             self.raw_data = self.raw_data[1:]
 
-            # convert data to tensor of shape(rows, features)
-            self.tensor_data = torch.tensor(self.raw_data.iloc[:,[7,8]].values)
-        
+            # Convert data to tensor of shape(rows, features).
+            # pylint: disable=not-callable
+            self.tensor_data = torch.tensor(self.raw_data.iloc[:, [7, 8]].values)
+
         self.features = self.create_features(self.tensor_data)
-        print('len of features:', len(self.features))
+        print("len of features:", len(self.features))
 
     def __len__(self):
         return len(self.features)
@@ -51,44 +50,42 @@ class GPSTDataset(Dataset):
 
     def create_features(self, tensor_data):
         """
-        Returns a list of features of the form 
+        Returns a list of features of the form
         (input, input_raw, is_masked, target, seg_id, label).
         """
-        original_data_len = self.tensor_data.shape[0]
+        original_data_len = tensor_data.shape[0]
         seq_len = self.seq_len
-        
+
         if DEBUG:
-            print('original_data_len', original_data_len)
-            print('seq_len', seq_len)
+            print("original_data_len", original_data_len)
+            print("seq_len", seq_len)
 
         num_seqs = original_data_len // seq_len
         input_ids_all = np.arange(0, num_seqs * seq_len)
-        
+
         features = []
         for i in range(num_seqs):
-            inputs_raw = self.tensor_data[i * seq_len: (i + 1) * seq_len]
-            input_ids = input_ids_all[i * seq_len: (i + 1) * seq_len]
+            inputs_raw = tensor_data[i * seq_len:(i + 1) * seq_len]
+            input_ids = input_ids_all[i * seq_len:(i + 1) * seq_len]
             position_ids = np.arange(0, seq_len)
             lm_labels = copy.deepcopy(input_ids)
             targets_raw = copy.deepcopy(inputs_raw)
-            features.append((input_ids, position_ids, lm_labels, inputs_raw, targets_raw))      
-        
+            features.append(
+                (input_ids, position_ids, lm_labels, inputs_raw, targets_raw))
+
         return features
 
+
 class GPSTEvalDataset(Dataset):
-    def __init__(self, 
-                 tensor_data, 
-                 seq_len, 
-                 encoding="utf-8", 
-                 on_memory=True):
+    def __init__(self, tensor_data, seq_len, encoding="utf-8", on_memory=True):
 
         self.seq_len = seq_len
 
         self.on_memory = on_memory
         self.encoding = encoding
-        self.tensor_data = tensor_data 
+        self.tensor_data = tensor_data
         self.features = self.create_features(self.tensor_data)
-        print('len of features:', len(self.features))
+        print("len of features:", len(self.features))
 
     def __len__(self):
         return len(self.features)
@@ -98,26 +95,27 @@ class GPSTEvalDataset(Dataset):
 
     def create_features(self, tensor_data):
         """
-        Returns a list of features of the form 
+        Returns a list of features of the form
         (input, input_raw, is_masked, target, seg_id, label).
         """
-        original_data_len = self.tensor_data.shape[0]
+        original_data_len = tensor_data.shape[0]
         seq_len = self.seq_len
-        
+
         if DEBUG:
-            print('original_data_len', original_data_len)
-            print('seq_len', seq_len)
+            print("original_data_len", original_data_len)
+            print("seq_len", seq_len)
 
         num_seqs = original_data_len // seq_len
         input_ids_all = np.arange(0, num_seqs * seq_len)
-        
+
         features = []
         for i in range(num_seqs):
-            inputs_raw = self.tensor_data[i * seq_len: (i + 1) * seq_len]
-            input_ids = input_ids_all[i * seq_len: (i + 1) * seq_len]
+            inputs_raw = tensor_data[i * seq_len:(i + 1) * seq_len]
+            input_ids = input_ids_all[i * seq_len:(i + 1) * seq_len]
             position_ids = np.arange(0, seq_len)
             lm_labels = copy.deepcopy(input_ids)
             targets_raw = copy.deepcopy(inputs_raw)
-            features.append((input_ids, position_ids, lm_labels, inputs_raw, targets_raw))      
-        
+            features.append(
+                (input_ids, position_ids, lm_labels, inputs_raw, targets_raw))
+
         return features
--- spred/gpst/train.py
+++ spred/gpst/train.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-# Copyright 2018 Google AI, Google Brain and Carnegie Mellon University Authors and the HuggingFace Inc. team.
+# Copyright 2018 Google AI, Google Brain and Carnegie Mellon
+# University Authors and the HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,105 +16,117 @@
 # limitations under the License.
 """ OpenAI GPT model fine-tuning script.
     Adapted from https://github.com/huggingface/pytorch-openai-transformer-lm/blob/master/train.py
-    It self adapted from https://github.com/openai/finetune-transformer-lm/blob/master/train.py
+    Itself adapted from https://github.com/openai/finetune-transformer-lm/blob/master/train.py
 
-    This script with default values fine-tunes and evaluate a pretrained OpenAI GPT on the RocStories dataset:
-        python run_openai_gpt.py \
-          --model_name openai-gpt \
-          --do_train \
-          --do_eval \
-          --train_dataset $ROC_STORIES_DIR/cloze_test_val__spring2016\ -\ cloze_test_ALL_val.csv \
-          --eval_dataset $ROC_STORIES_DIR/cloze_test_test__spring2016\ -\ cloze_test_ALL_test.csv \
-          --output_dir ../log \
-          --train_batch_size 16 \
+    This script with default values pretrains an OpenAI GPT model on a test dataset:
+        python train.py
+          --model_name openai-gpt
+          --do_train
+          --do_eval
+          --train_dataset
+          --eval_dataset
+          --output_dir ../log
+          --train_batch_size 16
 """
-import argparse
 import os
-import csv
 import random
 import logging
-from tqdm import tqdm, trange
+import argparse
 
 import numpy as np
+
+from tqdm import tqdm, trange
+from pytorch_transformers import AdamW, WarmupLinearSchedule, WEIGHTS_NAME, CONFIG_NAME
+
+# pylint: disable=wrong-import-order
 import torch
 from torch.utils.data import DataLoader
-
 if torch.__version__[:5] == "0.3.1":
     from torch.autograd import Variable
     from torch_addons.sampler import RandomSampler
 else:
+    # pylint: ungrouped-imports
     from torch.utils.data import RandomSampler
 
-from pytorch_transformers import (AdamW, WarmupLinearSchedule, cached_path, WEIGHTS_NAME,
-                                  CONFIG_NAME)
+# pylint: disable=wrong-import-position
 from dataset import GPSTDataset
 from modeling_openai import OpenAIGPTLMHeadModel, OpenAIGPTConfig
 
 DEBUG = False
 
-ROCSTORIES_URL = "https://s3.amazonaws.com/datasets.huggingface.co/ROCStories.tar.gz"
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+# pylint: disable=invalid-name
 logger = logging.getLogger(__name__)
+
 
 def accuracy(out, labels):
     outputs = np.argmax(out, axis=1)
     return np.sum(outputs == labels)
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name',
+    parser.add_argument("--model_name",
                         type=str,
-                        default='openai-gpt',
-                        help='pretrained model name')
-    parser.add_argument("--do_train", action='store_true', help="Whether to run training.")
+                        default="openai-gpt",
+                        help="pretrained model name")
+    parser.add_argument("--do_train",
+                        action="store_true",
+                        help="Whether to run training.")
     parser.add_argument("--do_eval",
-                        action='store_true',
+                        action="store_true",
                         help="Whether to run eval on the dev set.")
     parser.add_argument(
         "--output_dir",
         default=None,
         type=str,
         required=True,
-        help="The output directory where the model predictions and checkpoints will be written.")
-    parser.add_argument('--train_dataset', type=str, default='')
-    parser.add_argument('--eval_dataset', type=str, default='')
-    parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--num_train_epochs', type=int, default=3)
-    parser.add_argument('--train_batch_size', type=int, default=8)
-    parser.add_argument('--eval_batch_size', type=int, default=16)
-    parser.add_argument('--max_grad_norm', type=int, default=1)
-    parser.add_argument('--learning_rate', type=float, default=6.25e-5)
-    parser.add_argument('--warmup_proportion', type=float, default=0.002)
-    parser.add_argument('--lr_schedule', type=str, default='warmup_linear')
-    parser.add_argument('--weight_decay', type=float, default=0.01)
-    parser.add_argument('--lm_coef', type=float, default=0.9)
-    parser.add_argument('--n_valid', type=int, default=374)
-    parser.add_argument('--server_ip',
+        help="The output directory where the model \
+        predictions and checkpoints will be written.",
+    )
+    parser.add_argument("--train_dataset", type=str, default="")
+    parser.add_argument("--eval_dataset", type=str, default="")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--num_train_epochs", type=int, default=3)
+    parser.add_argument("--train_batch_size", type=int, default=8)
+    parser.add_argument("--eval_batch_size", type=int, default=16)
+    parser.add_argument("--max_grad_norm", type=int, default=1)
+    parser.add_argument("--learning_rate", type=float, default=6.25e-5)
+    parser.add_argument("--warmup_proportion", type=float, default=0.002)
+    parser.add_argument("--lr_schedule", type=str, default="warmup_linear")
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--lm_coef", type=float, default=0.9)
+    parser.add_argument("--n_valid", type=int, default=374)
+    parser.add_argument("--server_ip",
                         type=str,
-                        default='',
+                        default="",
                         help="Can be used for distant debugging.")
-    parser.add_argument('--server_port',
+    parser.add_argument("--server_port",
                         type=str,
-                        default='',
+                        default="",
                         help="Can be used for distant debugging.")
 
     # Added.
-    parser.add_argument("--gpst_model",
-                        default=None,
-                        type=str,
-                        required=True,
-                        help="OpenAIGPT pre-trained model path")
+    parser.add_argument(
+        "--gpst_model",
+        default=None,
+        type=str,
+        required=True,
+        help="OpenAIGPT pre-trained model path",
+    )
     args = parser.parse_args()
     print(args)
 
     if args.server_ip and args.server_port:
-        # Distant debugging - see https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
         import ptvsd
+
         print("Waiting for debugger attach")
-        ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
+        ptvsd.enable_attach(address=(args.server_ip, args.server_port),
+                            redirect_output=True)
         ptvsd.wait_for_attach()
 
     random.seed(args.seed)
@@ -121,10 +134,10 @@ def main():
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 
-    if not torch.__version__[:5] == "0.3.1":
+    if torch.__version__[:5] != "0.3.1":
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n_gpu = torch.cuda.device_count()
-    if not torch.__version__[:5] == "0.3.1":
+    if torch.__version__[:5] != "0.3.1":
         logger.info("device: {}, n_gpu {}".format(device, n_gpu))
 
     if not args.do_train and not args.do_eval:
@@ -154,27 +167,33 @@ def main():
     # Prepare optimizer
     if args.do_train:
         param_optimizer = list(model.named_parameters())
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        optimizer_grouped_parameters = [{
-            'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-            'weight_decay':
-            0.01
-        }, {
-            'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-            'weight_decay':
-            0.0
-        }]
+        no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params":
+                [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+                "weight_decay":
+                0.01,
+            },
+            {
+                "params":
+                [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
         num_train_optimization_steps = len(train_dataloader) * args.num_train_epochs
         optimizer = AdamW(
             optimizer_grouped_parameters,
             lr=args.learning_rate,
-                                                                                      #warmup=args.warmup_proportion,
-                                                                                      #max_grad_norm=args.max_grad_norm,
-            weight_decay=args.weight_decay)
-        scheduler = WarmupLinearSchedule(optimizer,
-                                         warmup_steps=(args.warmup_proportion *
-                                                       num_train_optimization_steps),
-                                         t_total=num_train_optimization_steps)
+            # warmup=args.warmup_proportion,
+            # max_grad_norm=args.max_grad_norm,
+            weight_decay=args.weight_decay,
+        )
+        scheduler = WarmupLinearSchedule(
+            optimizer,
+            warmup_steps=(args.warmup_proportion * num_train_optimization_steps),
+            t_total=num_train_optimization_steps,
+        )
 
     if args.do_train:
         nb_tr_steps, tr_loss, exp_average_loss = 0, 0, None
@@ -183,7 +202,7 @@ def main():
             tr_loss = 0
             nb_tr_steps = 0
             tqdm_bar = tqdm(train_dataloader, desc="Training")
-            for step, batch in enumerate(tqdm_bar):
+            for _, batch in enumerate(tqdm_bar):
                 if torch.__version__[:5] == "0.3.1":
                     batch = tuple(t.cuda() for t in batch)
                 else:
@@ -196,7 +215,11 @@ def main():
                 assert input_ids.shape == (args.train_batch_size, max_length)
                 assert position_ids.shape == (args.train_batch_size, max_length)
                 assert lm_labels.shape == (args.train_batch_size, max_length)
-                assert inputs_raw.shape == (args.train_batch_size, max_length, inputs_raw.shape[2])
+                assert inputs_raw.shape == (
+                    args.train_batch_size,
+                    max_length,
+                    inputs_raw.shape[2],
+                )
 
                 # torch_0.3.1 casting.
                 if torch.__version__[:5] == "0.3.1":
@@ -215,7 +238,8 @@ def main():
                         print("Type of targets_raw data:", type(targets_raw.data))
 
                 # Forward call.
-                outputs = model(input_ids, position_ids, None, lm_labels, inputs_raw, targets_raw)
+                outputs = model(input_ids, position_ids, None, lm_labels, inputs_raw,
+                                targets_raw)
                 loss = outputs[0]
                 loss.backward()
                 scheduler.step()
@@ -225,22 +249,23 @@ def main():
                 if torch.__version__[:5] == "0.3.1":
                     loss_data = float(loss.data)
                     tr_loss += loss_data
-                    exp_average_loss = loss_data if exp_average_loss is None else 0.7 * exp_average_loss + 0.3 * loss_data
+                    exp_average_loss = (loss_data if exp_average_loss is None else
+                                        0.7 * exp_average_loss + 0.3 * loss_data)
                 else:
                     tr_loss += loss.item()
-                    exp_average_loss = loss.item(
-                    ) if exp_average_loss is None else 0.7 * exp_average_loss + 0.3 * loss.item()
+                    exp_average_loss = (loss.item() if exp_average_loss is None else
+                                        0.7 * exp_average_loss + 0.3 * loss.item())
 
                 nb_tr_steps += 1
                 tqdm_bar.desc = "Training loss: {:.2e}".format(exp_average_loss)
 
-    # Save a trained model
+    # Save model and test loading functionality.
     if args.do_train:
-        # Save a trained model, configuration and tokenizer
-        model_to_save = model.module if hasattr(model,
-                                                'module') else model   # Only save the model it-self
 
-        # If we save using the predefined names, we can load using `from_pretrained`
+        # Only save the model itself.
+        model_to_save = model.module if hasattr(model, "module") else model
+
+        # If we save using the predefined names, we can load using ``from_pretrained``.
         output_model_file = os.path.join(args.output_dir, WEIGHTS_NAME)
         output_config_file = os.path.join(args.output_dir, CONFIG_NAME)
 
@@ -257,5 +282,6 @@ def main():
             model = OpenAIGPTLMHeadModel.from_pretrained(args.output_dir)
             model.to(device)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
