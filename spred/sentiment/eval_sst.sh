535dfb12c10d2b4061a36324968854cd885f6e23
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-12 13:33:37 -0400

    Small eval changes.

--- spred/sentiment/eval_sst.sh
+++ spred/sentiment/eval_sst.sh
@@ -6,7 +6,7 @@ test_datasets="sst"
 MODEL_ROOT="checkpoints"
 # Requires absolute path.
 # BERT_PATH="/home/mckade/spred/mt-dnn/scripts/checkpoints/sst_finetune/model_0.pt"
-BERT_PATH="/homes/3/whitaker.213/packages/mt-dnn/mt_dnn_models/mt_dnn_base_uncased.pt"
+BERT_PATH="/homes/3/whitaker.213/packages/data/mt-dnn/mt_dnn_models/model_0.pt"
 DATA_DIR="data/mt-dnn_uncased/"
 
 answer_opt=0
