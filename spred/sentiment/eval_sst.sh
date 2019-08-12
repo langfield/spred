37f8eec8f436e3adbf23240f307fc51af9d4a1a1
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-12 15:16:57 -0400

    Refactored eval_sst.sh.

--- spred/sentiment/eval_sst.sh
+++ spred/sentiment/eval_sst.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-prefix="mt-dnn-sst"
-tstr=$(date +"%FT%H%M")
+PREFIX="mt-dnn-sst"
+TSTR=$(date +"%FT%H%M")
 
-test_datasets="sst"
 MODEL_ROOT="checkpoints"
-# Requires absolute path.
-# BERT_PATH="/home/mckade/spred/mt-dnn/scripts/checkpoints/sst_finetune/model_0.pt"
-# BERT_PATH="/homes/3/whitaker.213/packages/data/mt-dnn/mt_dnn_models/model_0.pt"
 BERT_PATH="checkpoints/sst_finetune/model_0.pt"
 DATA_DIR="data/mt_dnn_uncased"
+MODEL_DIR="checkpoints/${PREFIX}_eval_${TSTR}"
+TWEET_DATA_DIR="../../../TweetScraper/Data/tweet/"
+LOG_FILE="${MODEL_DIR}/log.log"
+CUDA="True"
 
-model_dir="checkpoints/${prefix}_eval_${tstr}"
-log_file="${model_dir}/log.log"
-python3 eval.py --init_checkpoint ${BERT_PATH} --output_dir ${model_dir} --log_file ${log_file} --cuda "True"
+python3 eval.py --init_checkpoint ${BERT_PATH} --output_dir ${MODEL_DIR} --log_file ${LOG_FILE} --cuda ${CUDA} --tweet_data_dir ${TWEET_DATA_DIR}
