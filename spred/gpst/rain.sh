44f574931618d1c31d4410fe5b7ad97523c0ab43
McKade Umbenhower <mckade22@gmail.com>
2019-08-14 16:09:21 -0400

    modified rain.sh

--- spred/gpst/rain.sh
+++ spred/gpst/rain.sh
@@ -3,5 +3,9 @@
 MODEL_ROOT="checkpoints"
 GPST_MODEL="config.json"
 
-srun -J gpst -w vibranium --mem 10000 -c 4 python3 train.py --train_dataset sample_data.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 128 --num_train_epochs 30000
-# python3 train.py --train_dataset sample_data.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 128 --num_train_epochs 30000
+if [ "$1" -ne 1 ]; then
+    srun -J gpst -w vibranium --mem 10000 -c 4 python3 train.py --train_dataset sample_data.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 128 --num_train_epochs 30000
+else
+    python3 train.py --train_dataset sample_data.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 128 --num_train_epochs 30000
+fi
+
