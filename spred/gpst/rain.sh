ccbd99e1ca67fe5f2b28b82646f0b2d2aa3c738d
McKade Umbenhower <mckade22@gmail.com>
2019-08-15 16:45:41 -0400

    config

--- spred/gpst/rain.sh
+++ spred/gpst/rain.sh
@@ -6,6 +6,6 @@ GPST_MODEL="config.json"
 if [ "$(whoami)" != "mckade" ]; then
     srun -J gpst -w vibranium --mem 10000 -c 4 python3 train.py --train_dataset ../exchange/ETHUSDT_small_drop.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 8 --num_train_epochs 10 --save_freq 20
 else
-    python3 train.py --train_dataset ../exchange/ETHUSDT_small_drop.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 8 --num_train_epochs 10 --save_freq 20
+    python3 train.py --train_dataset ../../../ETHUSDT_drop.csv --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --do_train --train_batch_size 2048 --num_train_epochs 1500 --save_freq 20
 fi
 
