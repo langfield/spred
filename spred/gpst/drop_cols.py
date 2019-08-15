83b3c8ee19dba5297ea284aaf38e8b9944d36536
McKade Umbenhower <mckade22@gmail.com>
2019-08-15 14:49:20 -0400

    fixed drop indexed output

--- spred/gpst/drop_cols.py
+++ spred/gpst/drop_cols.py
@@ -8,7 +8,7 @@ parser.add_argument("--output_file", type=str, default="file")
 args = parser.parse_args()
 
 # Grab training data.
-raw_data = pd.read_csv(args.file)
+raw_data = pd.read_csv(args.file, sep=',')
 # drop all columns specified in the comma separated list from args
 drop = [x.strip() for x in args.drop_cols.split(',')]
 print("Dropping", drop)
@@ -18,4 +18,4 @@ if len(drop) > 0:
 print()
 print(raw_data.head())
 print("\nSaving to", args.output_file)
-raw_data.to_csv(path_or_buf=args.output_file, sep="\t")
+raw_data.to_csv(path_or_buf=args.output_file, sep="\t", index=False)
