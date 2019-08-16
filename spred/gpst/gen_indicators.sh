77056ad72c2a5acfad5e4e82c65dbd0cf05850a1
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-16 13:47:07 -0400

    Trained 2 models.

--- spred/gpst/gen_indicators.sh
+++ spred/gpst/gen_indicators.sh
@@ -1,1 +1,1 @@
-python3 gen_indicators.py --file "../exchange/ETHUSDT_small_drop.csv" --output_file "../exchange/ETHUSDT_small_ta.csv"
+python3 gen_indicators.py --file "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_file "../exchange/concatenated_price_data/ETHUSDT_ta.csv"
