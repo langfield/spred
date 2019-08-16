507213c903e30c7a8ffddb9f14cb31aa9cf9ff46
McKade Umbenhower <mckade22@gmail.com>
2019-08-16 13:46:49 -0400

    stationarize

--- spred/gpst/gen_indicators.sh
+++ spred/gpst/gen_indicators.sh
@@ -1,1 +1,1 @@
-python3 gen_indicators.py --file "../exchange/ETHUSDT_small_drop.csv" --output_file "../exchange/ETHUSDT_small_ta.csv"
+python3 gen_indicators.py --file "../../../ETHUSDT_drop.csv" --output_file "../../../ETHUSDT_ta.csv"
