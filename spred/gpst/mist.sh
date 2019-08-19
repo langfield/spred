3c09171711df1ff32cd1f3d9d698fd812cfa5444
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-19 18:59:36 -0400

    New graph.

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -1,1 +1,1 @@
-python3 eval.py --batch 1 --width 50 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+python3 eval.py --batch 1 --width 1000 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
