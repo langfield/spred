29be304c8f535ccc44ab007bab67f481a77f8036
McKade Umbenhower <mckade22@gmail.com>
2019-08-19 17:14:55 -0400

    mist

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -1,1 +1,7 @@
-python3 eval.py --batch 1 --width 50 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+#!/bin/bash
+
+if [ "$(whoami)" != "mckade" ]; then
+    python3 eval.py --batch 1 --width 50 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+else
+    python3 eval.py --batch 1 --width 50 --input "../../../ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+fi
