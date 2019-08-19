09b586f373a0af29e83c16bb8dab2aa452182c0a
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-19 17:00:28 -0400

    No more averaging.

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -1,1 +1,1 @@
-python3 eval.py --batch 1 --width 50 --input "../../../ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+python3 eval.py --batch 1 --width 50 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
