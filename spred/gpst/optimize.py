fe6648440a4f9ca79e342700e3060f644778731e
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-20 09:36:36 -0400

    New graph.

--- spred/gpst/optimize.py
+++ spred/gpst/optimize.py
@@ -20,31 +20,33 @@ def objective(trial: optuna.Trial) -> float:
     args = parser.parse_args()
 
     # Must cast args which are supposed to be ints to ints.
-    args.seed = trial.suggest_int("seed", 40, 43)
+    # args.seed = trial.suggest_int("seed", 40, 43)
+    args.seed = 42
     args.train_batch_size = int(trial.suggest_discrete_uniform(
-        "train_batch_size", 32, 128, 16
+        "train_batch_size", 16, 64, 8
     ))
-    args.max_grad_norm = trial.suggest_int("max_grad_norm", 1, 5)
-    args.learning_rate = trial.suggest_loguniform("learning_rate", 2.5e-5, 5e-3)
-    args.warmup_steps = int(trial.suggest_discrete_uniform(
-        "warmup_steps", 10000, 60000, 10000
-    ))
+    # args.train_batch_size = 32
+    
+    # args.max_grad_norm = trial.suggest_int("max_grad_norm", 1, 5)
+    args.max_grad_norm = 3
+    args.learning_rate = trial.suggest_loguniform("learning_rate", 1e-7, 1e-4)
+    args.warmup_steps = 10000
     args.warmup_proportion = trial.suggest_uniform(
         "warmup_proportion", 0.05, 0.4
     )
-    args.weight_decay = trial.suggest_loguniform("weight_decay", 1e-3, 3e-2)
-    args.adam_epsilon = trial.suggest_loguniform("adam_epsilon", 1e-8, 1e-7)
+    args.weight_decay = trial.suggest_loguniform("weight_decay", 5e-4, 1e-2)
+    args.adam_epsilon = trial.suggest_loguniform("adam_epsilon", 1e-9, 1e-7)
 
     config = {}
-    config["initializer_range"] = trial.suggest_uniform("initializer_range", 0.02, 0.05)
+    config["initializer_range"] = trial.suggest_uniform("initializer_range", 0.01, 0.05)
     config["layer_norm_epsilon"] = trial.suggest_loguniform(
         "layer_norm_epsilon", 1e-12, 5e-5
     )
-    config["n_ctx"] = int(trial.suggest_discrete_uniform("n_ctx", 30, 180, 10))
+    config["n_ctx"] = int(trial.suggest_discrete_uniform("n_ctx", 5, 60, 5))
     config["n_positions"] = config["n_ctx"]
-    config["resid_pdrop"] = trial.suggest_uniform("resid_pdrop", 0.05, 0.15)
-    config["attn_pdrop"] = trial.suggest_uniform("attn_pdrop", 0.05, 0.15)
-    config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 128, 768, 128))
+    config["resid_pdrop"] = trial.suggest_uniform("resid_pdrop", 0.02, 0.15)
+    config["attn_pdrop"] = trial.suggest_uniform("attn_pdrop", 0.02, 0.15)
+    config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 64, 256, 32))
     config["n_head"] = 16
     config["vocab_size"] = 5  # Input data dim.
 
