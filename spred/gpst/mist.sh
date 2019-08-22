7c7a93a569d30b71e776d3a2db90f4059e8700aa
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-21 21:57:28 -0400

    'mist.sh' refactored.

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$(whoami)" != "mckade" ]; then
-    python3 eval.py --batch 1 --width 50 --input "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --normalize
+    python3 eval.py --eval_batch_size 1 --width 50 --eval_dataset "../exchange/concatenated_price_data/ETHUSDT_drop.csv" --graph_dir "graphs/" --terminal_plot_width 50 --normalize
 else
-    python3 eval.py --batch 1 --width 50 --input "../../../ETHUSDT_drop.csv" --output_dir "graphs/" --terminal_plot_width 50 --stationarize
+    python3 eval.py --eval_batch_size 1 --width 50 --eval_dataset "../../../ETHUSDT_drop.csv" --graph_dir "graphs/" --terminal_plot_width 50 --stationarize
 fi
 
