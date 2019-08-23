28146d69dc9f6c2c9c3562412c2d73e8cf7def6b
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-23 18:46:21 -0400

    XRP.

--- spred/gpst/rop_cols.sh
+++ spred/gpst/rop_cols.sh
@@ -1,2 +1,2 @@
-python3 drop_cols.py --file "../exchange/concatenated_price_data/IOTABTC.csv" --output_file "../exchange/concatenated_price_data/IOTABTC_drop.csv" --drop_cols "Date"
+python3 drop_cols.py --file "../exchange/concatenated_price_data/XRPBTC.csv" --output_file "../exchange/concatenated_price_data/XRPBTC_drop.csv" --drop_cols "Date"
 # python3 drop_cols.py --file "../../../ETHUSDT_ta.csv" --output_file "../../../ETHUSDT_ta_drop.csv" --drop_cols "trend_adx, trend_adx_pos, trend_adx_neg"
