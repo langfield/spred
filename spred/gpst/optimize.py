223ee28cfdf833c32e5f4b5d2b40101868719e8d
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-23 13:07:56 -0400

    Results of non-stationarized, non-aggregated optuna run.

--- spred/gpst/optimize.py
+++ spred/gpst/optimize.py
@@ -37,30 +37,32 @@ def objective(trial: optuna.Trial) -> float:
 
     # Set arguments.
     args.num_train_epochs = 100000
-    args.stationarize = False
+    args.stationarize = True
     args.normalize = True
-    args.aggregation_size = 1
     args.seed = 42
     args.max_grad_norm = 3
     args.warmup_steps = 10000
-    args.learning_rate = trial.suggest_loguniform("learning_rate", 1e-7, 1e-4)
+    args.weight_decay = 0.005
+    args.adam_epsilon = 7.400879524874149e-08
+    args.learning_rate = trial.suggest_loguniform("learning_rate", 8e-7, 5e-4)
     args.warmup_proportion = trial.suggest_uniform("warmup_proportion", 0.05, 0.4)
-    args.weight_decay = trial.suggest_loguniform("weight_decay", 5e-4, 1e-2)
-    args.adam_epsilon = trial.suggest_loguniform("adam_epsilon", 1e-9, 1e-7)
-    batch_size = trial.suggest_discrete_uniform("train_batch_size", 64, 512, 64)
+    batch_size = trial.suggest_discrete_uniform("train_batch_size", 32, 256, 32)
     args.train_batch_size = int(batch_size)
+    agg_size = trial.suggest_discrete_uniform("agg_size", 5, 30, 5)
+    args.aggregation_size = int(agg_size)
 
     # Set config.
     config = {}
-    config["initializer_range"] = trial.suggest_uniform("initializer_range", 0.01, 0.05)
-    config["layer_norm_epsilon"] = trial.suggest_loguniform("lay_norm_eps", 1e-12, 5e-5)
-    config["n_ctx"] = int(trial.suggest_discrete_uniform("n_ctx", 5, 60, 5))
+    config["initializer_range"] = 0.039589260915990014
+    config["layer_norm_epsilon"] = 9.064249722000914e-11
+    config["n_ctx"] = int(trial.suggest_discrete_uniform("n_ctx", 5, 30, 5))
     config["n_positions"] = config["n_ctx"]
-    config["resid_pdrop"] = trial.suggest_uniform("resid_pdrop", 0.02, 0.15)
-    config["attn_pdrop"] = trial.suggest_uniform("attn_pdrop", 0.02, 0.15)
-    config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 32, 256, 16))
+    config["resid_pdrop"] = 0.08
+    config["attn_pdrop"] = 0.08
+    config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 32, 768, 64))
     config["n_head"] = 16
     config["vocab_size"] = 5
+    config["n_layer"] = 12
 
     dirpath = tempfile.mkdtemp()
     config_filename = str(time.time()) + ".json"
