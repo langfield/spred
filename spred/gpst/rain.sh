7a83419ef0f1ea08ca7c5e09a2f11e88a73fbc47
McKade Umbenhower <mckade22@gmail.com>
2019-08-22 22:02:18 -0400

    debugging eval

--- spred/gpst/rain.sh
+++ spred/gpst/rain.sh
@@ -22,6 +22,11 @@ NORMALIZE="--normalize"
 if [ "$(whoami)" != "mckade" ]; then
     srun -J gpst -w adamantium --mem 10000 -c 4 python3 train.py --dataset ${DATASET} --gpst_model ${GPST_MODEL} --output_dir ${OUTPUT_DIR} --model_name ${MODEL_NAME}  --seed ${SEED} --train_batch_size ${TRAIN_BATCH_SIZE} --num_train_epochs ${NUM_TRAIN_EPOCHS} --save_freq ${SAVE_FREQ} --max_grad_norm ${MAX_GRAD_NORM} --learning_rate ${LEARNING_RATE} --warmup_proportion ${WARMUP_PROPORTION} --weight_decay ${WEIGHT_DECAY} --adam_epsilon ${ADAM_EPSILON} --aggregation_size ${AGGREGATION_SIZE} ${STATIONARIZE} ${NORMALIZE}
 else
+    NORMALIZE=""
+    DATASET="../../../ETHUSDT_drop.csv"
+    AGGREGATION_SIZE="30"
+    NUM_TRAIN_EPOCHS="10000"
+    SAVE_FREQ="100"
     python3 train.py --dataset ${DATASET} --gpst_model ${GPST_MODEL} --output_dir ${OUTPUT_DIR} --model_name ${MODEL_NAME}  --seed ${SEED} --train_batch_size ${TRAIN_BATCH_SIZE} --num_train_epochs ${NUM_TRAIN_EPOCHS} --save_freq ${SAVE_FREQ} --max_grad_norm ${MAX_GRAD_NORM} --learning_rate ${LEARNING_RATE} --warmup_proportion ${WARMUP_PROPORTION} --weight_decay ${WEIGHT_DECAY} --adam_epsilon ${ADAM_EPSILON} --aggregation_size ${AGGREGATION_SIZE} ${STATIONARIZE} ${NORMALIZE}
 fi
 
