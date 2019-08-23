cf8004ad11e230a56b311903949570102e95888c
McKade Umbenhower <mckade22@gmail.com>
2019-08-23 15:38:08 -0400

    eval fix

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -19,8 +19,9 @@ if [ "$(whoami)" != "mckade" ]; then
     python3 eval.py --gpst_model ${GPST_MODEL} --output_dir ${OUTPUT_DIR} --model_name ${MODEL_NAME} --eval_batch_size ${EVAL_BATCH_SIZE} --width ${WIDTH} --dataset ${DATASET} --graph_dir ${GRAPH_DIR} --terminal_plot_width ${TERMINAL_PLOT_WIDTH} ${NORMALIZE} ${STATIONARIZE} --aggregation_size ${AGGREGATION_SIZE}
 else
     DATASET="../../../ETHUSDT_drop.csv"
-    AGGREGATION_SIZE="1"
+    AGGREGATION_SIZE="30"
     STATIONARIZE="--stationarize"
+    NORMALIZE=""
     python3 eval.py --gpst_model ${GPST_MODEL} --output_dir ${OUTPUT_DIR} --model_name ${MODEL_NAME} --eval_batch_size ${EVAL_BATCH_SIZE} --width ${WIDTH} --dataset ${DATASET} --graph_dir ${GRAPH_DIR} --terminal_plot_width ${TERMINAL_PLOT_WIDTH} ${NORMALIZE} ${STATIONARIZE} --aggregation_size ${AGGREGATION_SIZE}
 fi
 
