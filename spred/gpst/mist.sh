d0bae09eb619b2f18341861441f00622021e902a
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-31 17:41:27 -0400

    Restructuring repo.

--- spred/gpst/mist.sh
+++ spred/gpst/mist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# DATASET="../exchange/concatenated_price_data/ETHUSDT_prepro.csv"
-DATASET="../gemini_prepro.csv"
+DATASET="../exchange/gemini_prepro.csv"
 GPST_MODEL="config.json"
 MODEL_NAME="optuna"
 OUTPUT_DIR="checkpoints"
@@ -21,7 +20,7 @@ SEQ_NORM="--seq_norm"
 if [ "$(whoami)" != "mckade" ]; then
     python3 eval.py --gpst_model ${GPST_MODEL} --output_dir ${OUTPUT_DIR} --model_name ${MODEL_NAME} --eval_batch_size ${EVAL_BATCH_SIZE} --width ${WIDTH} --dataset ${DATASET} --graph_dir ${GRAPH_DIR} --terminal_plot_width ${TERMINAL_PLOT_WIDTH} ${NORMALIZE} ${STATIONARIZE} --aggregation_size ${AGGREGATION_SIZE} ${SEQ_NORM}
 else
-    DATASET="../gemini.csv"
+    DATASET="../exchange/gemini.csv"
     AGGREGATION_SIZE="1"
     STATIONARIZE="--stationarize"
     NORMALIZE=""
