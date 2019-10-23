a22223249c8a54ad882d74081a823266c27f6ce9
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-10-23 21:47:32 +0000

    Optimize update.

--- spred/gpst/snow.sh
+++ spred/gpst/snow.sh
@@ -3,7 +3,7 @@
 MODEL_ROOT="ckpts"
 GPST_MODEL="config.json"
 DATASET="/root/books/sampleset.csv"
-TIMEOUT="3600"
+TIMEOUT="600"
 SAVE_FREQ="20"
 
 python3 optimize.py --dataset ${DATASET} --gpst_model ${GPST_MODEL} --output_dir ${MODEL_ROOT} --save_freq ${SAVE_FREQ} --timeout ${TIMEOUT}
