11ec8b5c7a3a88b119d945881b9a1084bdc3c74c
Brendan Whitaker <whitaker.213@osu.edu>
2019-11-13 19:02:18 +0000

    Hyperparam search for smaller model.

--- spred/gpst/optimize.py
+++ spred/gpst/optimize.py
@@ -62,14 +62,15 @@ def objective(trial: optuna.Trial) -> float:
     args.warmup_proportion = 0.0
     args.sep = ","
 
-    batch_size = 192
+    batch_size = 64
+    n_positions = 30
     agg_size = 1
 
     # Commented-out trial suggestions should be placed at top of block.
     # args.stationarize = trial.suggest_categorical("stationarize", [True, False])
     # agg_size = trial.suggest_discrete_uniform("agg_size", 1, 40, 5)
     # args.warmup_proportion = trial.suggest_uniform("warmup_proportion", 0.05, 0.4)
-    batch_size = trial.suggest_discrete_uniform("train_batch_size", 4, 64, 4)
+    # batch_size = trial.suggest_discrete_uniform("train_batch_size", 4, 64, 4)
     args.weight_decay = trial.suggest_loguniform("weight_decay", 0.0001, 0.01)
     args.learning_rate = trial.suggest_loguniform("learning_rate", 8e-7, 5e-3)
     args.train_batch_size = int(batch_size)
@@ -80,9 +81,11 @@ def objective(trial: optuna.Trial) -> float:
     config = {}
     config["initializer_range"] = 0.02
     config["n_head"] = 8
+    config["n_embd"] = 256
+    config["n_layer"] = 6 
     config["input_dim"] = 300
     config["orderbook_depth"] = 6
-    config["horizon"] = 20
+    config["horizon"] = 30
     config["modes"] = [
         "bid_classification",
         "bid_increase",
@@ -91,16 +94,15 @@ def objective(trial: optuna.Trial) -> float:
         "ask_increase",
         "ask_decrease"
     ]
-    # n_positions = 30
 
     # Commented-out trial suggestions should be placed at top of block.
     # config["n_head"] = int(trial.suggest_discrete_uniform("n_head", 4, 16, 4))
+    # config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 64, 128, 8))
+    # config["n_layer"] = trial.suggest_int("n_layer", 4, 8)
     n_positions = int(trial.suggest_discrete_uniform("n_ctx", 60, 600, 30))
     config["layer_norm_epsilon"] = trial.suggest_loguniform("layer_eps", 1e-5, 1e-3)
     config["resid_pdrop"] = trial.suggest_loguniform("resid_pdrop", 0.01, 0.15)
     config["attn_pdrop"] = trial.suggest_loguniform("attn_pdrop", 0.1, 0.3)
-    config["n_embd"] = int(trial.suggest_discrete_uniform("n_embd", 64, 128, 8))
-    config["n_layer"] = trial.suggest_int("n_layer", 4, 8)
     config["initializer_range"] = trial.suggest_loguniform("initrange", 0.005, 0.04)
     config["n_positions"] = n_positions
     config["n_ctx"] = n_positions
