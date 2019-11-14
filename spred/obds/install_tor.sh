11b194922f13d04184ef5dcc0f691ca8c6f5074f
Brendan Whitaker <whitaker.213@osu.edu>
2019-11-14 00:49:09 +0000

    Todos, var name changes, install script finished.

--- spred/obds/install_tor.sh
+++ spred/obds/install_tor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+apt-get install tor
 echo "deb https://deb.torproject.org/torproject.org bionic main" >> /etc/apt/sources.list
 echo "deb-src https://deb.torproject.org/torproject.org bionic main" >> /etc/apt/sources.list
 curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
