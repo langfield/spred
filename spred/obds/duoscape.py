8e635f6f8aef0480a7324be8f9e17de8da5e29aa
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-12-16 17:54:09 -0500

    Recreated 'tor_request.py'. Multiscrape is now working and using n different ip addresses.
    Refactored multiscrape and duoscape files. Deleted a couple of old files used for testing multiprocessing and queue handling.
    Need to make sure timing is handled correctly, and determine if we should drop it down to only two request objects at a time.

--- spred/obds/duoscape.py
+++ spred/obds/duoscape.py
@@ -1,24 +1,13 @@
+""" Tests the modified TorRequest class. """
 #! /usr/bin/python3
-
 import os
-import sys
-import time
-import json
-import argparse
-import datetime
-import functools
-import multiprocessing as mp
-from time import mktime
-from queue import Full as QueueFull
-from queue import Empty as QueueEmpty
-from typing import List, Any, Dict, Tuple, Generator
-from contextlib import closing
-
 from tor_request import TorRequest
 
 # url = "https://api.cryptowat.ch/markets/kraken/ethusd/orderbook"
 
+
 def get_ip(tor):
+    """ Grab ip address of exit node. """
     url = "http://httpbin.org/ip"
     try:
         response = tor.get(url)
@@ -28,15 +17,20 @@ def get_ip(tor):
         print(exc)
         raise ValueError(str(exc))
 
-pid = os.fork()
-if pid == 0:
-    with TorRequest(proxy_port=9050, ctrl_port=9051, data_dir = './tordatac') as tor:
-        for i in range(10):
-            print('child: ', get_ip(tor)) # child
-else:
-    with TorRequest(proxy_port=9060, ctrl_port=9061, data_dir = './tordatap') as tor:
-        for i in range(10):
-            print('parent: ', get_ip(tor)) # parent
-    
-    
-    os.waitpid(pid, 0)
+
+def main():
+    """ Testing function. """
+    pid = os.fork()
+    if pid == 0:
+        with TorRequest(proxy_port=9050, ctrl_port=9051, data_dir="./tordatac") as tor:
+            for _ in range(10):
+                print("child: ", get_ip(tor))  # child
+    else:
+        with TorRequest(proxy_port=9060, ctrl_port=9061, data_dir="./tordatap") as tor:
+            for _ in range(10):
+                print("parent: ", get_ip(tor))  # parent
+
+        os.waitpid(pid, 0)
+
+if __name__ == "__main__":
+    main()
